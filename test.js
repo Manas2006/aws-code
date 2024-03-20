1f213b904f4fcf0cb4f8b07c4b1d26f4c23220f2
Manas2006 <60903543+Manas2006@users.noreply.github.com>
2024-03-20 09:33:10 -0500

    Update test.js
    node.js input

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
+const readline = require('readline');
+
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+let inputLines = [];
+
+
 r1.on('line', function(line) {
     if (line == '') {
         r1.close();
